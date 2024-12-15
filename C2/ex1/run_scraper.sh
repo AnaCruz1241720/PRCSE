3a89ab02e067897ae59a299955d777948d665389
AnaCruz1241720 <1241720@isep.ipp.pt>
2024-12-15 18:44:00 +0000

    Update run_scraper.sh

--- C2/ex1/run_scraper.sh
+++ C2/ex1/run_scraper.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-python3 ex1.py C2/ex1/generated > C2/ex1/generated/report_$(date +%Y-%m-%d).txt
+
+#check if the virtual environment exists and, if not, creates one
+if [ ! -d "/venv" ]; then    
+    python3 -m venv venv    
+fi
+
+#Activate virtual environment
+source venv/bin/activate
+
+#Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Create generated folder if it doesn't exist 
+mkdir -p generated
+
+#Run Scraper
+python3 ex1.py generated 40 > generated/report_$(date +%Y-%m-%d).txt
+
+deactivate
